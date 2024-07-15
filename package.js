1c938eb055c10f31f93fc5bff8c8cba1b689fd70
Jan Dvorak <jdvorak@literaryuniverse.com>
2024-07-15 21:17:59 +0200

    Meteor 3 version updates

--- package.js
+++ package.js
@@ -7,14 +7,14 @@ Package.describe({
 });
 
 Package.onUse(function _(api) {
-    api.versionsFrom(['2.8.1']);
+    api.versionsFrom(['2.8.1', '3.0']);
 
     api.use([
         'check',
         'reywood:publish-composite@1.8.9',
         'socialize:user-blocking@2.0.0',
         'socialize:requestable@2.0.0',
-        'aldeed:simple-schema@1.13.1'
+        'aldeed:simple-schema@1.13.1 || 2.0.0'
     ]);
 
     api.imply('socialize:user-blocking');
